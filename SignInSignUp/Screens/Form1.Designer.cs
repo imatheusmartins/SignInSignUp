e310ecbeb8e302384e063719a13027d46ff3e625
Matheus Martins <matheusjayaly@gmail.com>
2022-12-07 21:52:31 -0300

    Screens

---
+++ SignInSignUp/Screens/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SignInSignUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label lblCompany;
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            lblCompany = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCompany
+            // 
+            lblCompany.AutoSize = true;
+            lblCompany.Font = new System.Drawing.Font("Tahoma", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblCompany.ForeColor = System.Drawing.Color.Black;
+            lblCompany.Location = new System.Drawing.Point(104, 28);
+            lblCompany.Name = "lblCompany";
+            lblCompany.Size = new System.Drawing.Size(302, 48);
+            lblCompany.TabIndex = 0;
+            lblCompany.Text = "Oliveira Trade";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(203, 343);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(114, 39);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSignin.FlatAppearance.BorderSize = 0;
+            this.btnSignin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnSignin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSignin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignin.Location = new System.Drawing.Point(112, 270);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(114, 39);
+            this.btnSignin.TabIndex = 2;
+            this.btnSignin.Text = "SIGN IN ";
+            this.btnSignin.UseVisualStyleBackColor = false;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSignup.FlatAppearance.BorderSize = 0;
+            this.btnSignup.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignup.Location = new System.Drawing.Point(292, 270);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(114, 39);
+            this.btnSignup.TabIndex = 3;
+            this.btnSignup.Text = "SIGN UP";
+            this.btnSignup.UseVisualStyleBackColor = false;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(233, 132);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(173, 20);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(233, 189);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(173, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(107, 132);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(53, 26);
+            this.lblUser.TabIndex = 6;
+            this.lblUser.Text = "User";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(107, 183);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(99, 26);
+            this.lblPass.TabIndex = 7;
+            this.lblPass.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(523, 440);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnSignup);
+            this.Controls.Add(this.btnSignin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(lblCompany);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPass;
+    }
+}
+
